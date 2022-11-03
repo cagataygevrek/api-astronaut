ad8683c93a65a57642b567db49c50046d57eb823
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-11-03 15:29:24 +0300

    Kodlar düzenli hale getirildi. İşlemler açıklandı.

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
+// jsonBaglantiAdresi değişkeni oluşturulup fetch işlemi için ilgili api linki eklendi.
 let JsonBaglantiAdresi = fetch("http://api.open-notify.org/astros.json");
+// yanitAl değişkeni oluşturuldu ve js objesine dönüştürme işlemi başlatıldı.
 let yanitAl = JsonBaglantiAdresi.then((yanit) => yanit.json());
 
+//js objesi alındıktan sonra yapılacaklar eklendi.
 yanitAl.then((uzaydakiler) => {
+  // index.html dosyasındaki id'si root olan div seçildi.
   let rootEleman = document.querySelector("#root");
 
-  let isimSayisi = document.createElement("p");
-  isimSayisi.classList.add("kisisayisi");
-  isimSayisi.textContent = "Uzayda görevli kişi sayısı: " + uzaydakiler.number;
-  rootEleman.appendChild(isimSayisi);
-
-  for (var i = 1; i <= 10; i++) {
-    let imgEkle = document.createElement("img");
-    imgEkle.src = "https://www.cagataygevrek.com/odev18avatar.png";
-    imgEkle.classList.add("img");
-    rootEleman.appendChild(imgEkle);
-  }
-
+  // h2 elementi oluşturuldu ve baslikOlustur değişkenine aktarıldı.
   let baslikOlustur = document.createElement("h2");
+  //olusturulan h2 elementinin class'ı baslik olarak eklendi.
   baslikOlustur.classList.add("baslik");
+  // oluşturulan h2 elementinin içeriği güncellendi.
   baslikOlustur.innerHTML = "Görevli Listesi";
+  // baslikOlustur değişkeni root div'inin (rootEleman değişkeni) appendChild'ı olarak atandı.
   rootEleman.appendChild(baslikOlustur);
 
+  // p elementi oluşturuldu ve isimSayısı değişkenine aktarıldı.
+  let isimSayisi = document.createElement("p");
+  //oluşturulan p elementinin class'ı kisisayisi olarak eklendi.
+  isimSayisi.classList.add("kisisayisi");
+  //oluşturulan p elementinin içeriğine ilgili veriler eklendi.
+  isimSayisi.textContent = "Uzayda görevli kişi sayısı: " + uzaydakiler.number;
+  // isim sayısı root div'inin (rootEleman değişkeni) appendChild'ı olarak atandı.
+  rootEleman.appendChild(isimSayisi);
+
+  // for döngüsü ile seçmiş olduğumuz root div'ine json dosyasındaki veriler eklendi.
   for (i = 0; i < 10; i++) {
+    //img elementi oluşturuldu ve imgEkle değişkenine aktarıldı.
+    let imgEkle = document.createElement("img");
+    //oluşturulan imgEkle değişkeninin yani img elementinin adresine resim uzantısı eklendi.
+    imgEkle.src = "https://www.cagataygevrek.com/odev18avatar.png";
+    //oluşturulan imgEkle değişleninin class'ı img olarak eklendi.
+    imgEkle.classList.add("img");
+    // imgEkle değişkeni root div'inin (rootEleman değişkeni) appendChild'ı olarak atandı.
+    rootEleman.appendChild(imgEkle);
+
+    // h2 elementi oluşturuldu ve jsonName değişkenine aktarıldı.
     let jsonName = document.createElement("h2");
+    // oluşturulan h2 elementinin class'ı jsonCraft olarak eklendi.
     jsonName.classList.add("jsonName");
-    let jsonCraft = document.createElement("h4");
-    jsonCraft.classList.add("jsonCraft");
-    jsonName.textContent = uzaydakiler.people[i].name;
-    jsonCraft.textContent = "Bulunduğu istasyon:" + uzaydakiler.people[i].craft;
+    // jsonName değişkeni root div'inin (rootEleman değişkeni) appendChild'ı olarak atandı.
     rootEleman.appendChild(jsonName);
+
+    // h4 elementi oluşturuldu ve jsonName değişkenine aktarıldı.
+    let jsonCraft = document.createElement("h4");
+    //oluşturulan h4 elementinin class'ı jsonCraft olarak eklendi.
+    jsonCraft.classList.add("jsonCraft");
+    // jsonCraft değişkeni rootEleman değişkeninin appendChild'ı olarak güncellendi.
     rootEleman.appendChild(jsonCraft);
+
+    // jsonName değişkeninin içeriği json dosyasından alınan verilerle güncellendi.
+    jsonName.textContent = uzaydakiler.people[i].name;
+    // jsonCraft değişkeninin içeriği json dosyasından alınan verilerle güncellendi.
+    jsonCraft.textContent = "Bulunduğu istasyon:" + uzaydakiler.people[i].craft;
   }
 });
